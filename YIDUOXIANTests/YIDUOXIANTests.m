229e52a7037fc568d501ea00eb48ee7a89c4178c
张孟春 <Spring@zhangmengchundemac-mini.local>
2016-09-18 14:59:59 +0800

    Initial Commit

---
+++ YIDUOXIANTests/YIDUOXIANTests.m
@@ -0,0 +1,39 @@
+//
+//  YIDUOXIANTests.m
+//  YIDUOXIANTests
+//
+//  Created by 张孟春 on 16/9/18.
+//  Copyright © 2016年 张孟春. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YIDUOXIANTests : XCTestCase
+
+@end
+
+@implementation YIDUOXIANTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
